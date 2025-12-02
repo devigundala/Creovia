5d50f67a4afaea00cddefba36171ba3020f4bd8f
Devivaraprasad Gundala <devivaraprasadgundala@gmail.com>
2025-12-02 14:34:43 +0530

    Project completed

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import { clerkMiddleware, requireAuth } from '@clerk/express'
+import aiRouter from './routes/aiRoutes.js';
+import connectCloudinary from './configs/cloudinary.js';
+import userRouter from './routes/userRoutes.js';
+
+
+
+const app = express()
+
+await connectCloudinary()
+
+const Port = process.env.Port || 3000;
+
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
+app.get('/', (req,res)=>res.send('Server is Live!'))
+
+app.use(requireAuth())
+
+app.use('/api/ai',aiRouter)
+app.use('/api/user',userRouter)
+
+app.listen(Port,()=>{
+    console.log('Server is running on port',Port);
+})
+
